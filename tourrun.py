3cda035f8cebd8de0e2ccbe85b31e448e3bec57e
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-04-24 19:27:02 -0500

    Revert "maybe"
    This reverts commit 1e9a3f95ab1a36082f1bb7d4a71fa5a83854b238.

--- tourrun.py
+++ tourrun.py
@@ -227,6 +227,5 @@ if __name__ == "__main__":
     tour = Tournament(toururl, config["challonge_username"], config["challonge_api_key"])
     log.configure(console_level=logging.ERROR)
     loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     loop.run_until_complete(main(tour))
 
