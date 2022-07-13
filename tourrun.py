cf399b604888cdc5dfb522df1a2642f5469413fb
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-07-13 18:48:32 -0500

    dont lower

--- tourrun.py
+++ tourrun.py
@@ -61,7 +61,7 @@ def slugify(value, allow_unicode=False):
         value = unicodedata.normalize('NFKC', value)
     else:
         value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value.lower())
+    value = re.sub(r'[^\w\s-]', '', value)
     return re.sub(r'[-\s]+', '-', value).rstrip('-_')
 
 def get_latest_image():
