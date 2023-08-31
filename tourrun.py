65e2d8398494456e5682ea6c0b2b0857b4e3ef96
jozz024 <74272560+jozz024@users.noreply.github.com>
2023-08-31 13:26:55 -0500

    add print state,nents

--- tourrun.py
+++ tourrun.py
@@ -224,8 +224,10 @@ async def main(tour: Tournament, whole_thing: bool):
                 while True:
                     data = s.recv(1024)
                     try:
+                        print(data.decode())
                         if data.decode().startswith("[match_end] match_data_json: "):
                             data_json = json.loads(data.decode().strip("[match_end] match_data_json: ").lstrip())
+                            print(data_json["fp1_info"]["score"])
                             break
                         if data.decode().startswith("[match_end] One of the fighters is not an amiibo, exiting."):
                             await restart_match(controller_state, fp1_tag, fp2_tag)
