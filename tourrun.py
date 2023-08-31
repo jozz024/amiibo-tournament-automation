1b82c08dc059e159d2985246c3b32960885b4826
jozz024 <74272560+jozz024@users.noreply.github.com>
2023-08-31 15:11:46 -0500

    a

--- tourrun.py
+++ tourrun.py
@@ -226,6 +226,7 @@ async def main(tour: Tournament, whole_thing: bool):
                     # try:
                     print(data.decode())
                     if data.decode().startswith("[match_end] match_data_json: "):
+                        print(data.decode().strip("[match_end] match_data_json: ").lstrip().strip('"'))
                         data_json = json.loads(data.decode().strip("[match_end] match_data_json: ").lstrip().strip('"'))
                         print(data_json)
                         break
