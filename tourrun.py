6df80c404c163bc63749a6e41c796a9920dc9174
jozz024 <74272560+jozz024@users.noreply.github.com>
2023-08-31 15:25:12 -0500

    s

--- tourrun.py
+++ tourrun.py
@@ -227,7 +227,7 @@ async def main(tour: Tournament, whole_thing: bool):
                     print(data.decode())
                     if data.decode().startswith("[match_end] match_data_json: "):
                         print(data.decode().strip("[match_end] match_data_json: ").lstrip().strip('"'))
-                        data_json = json.loads(data.decode().strip("[match_end] match_data_json: ").lstrip().strip('"'))
+                        data_json = json.loads(data.decode().strip("[match_end] match_data_json: ").lstrip().strip('"').replace("\\", ""))
                         print(data_json)
                         break
                     if data.decode().startswith("[match_end] One of the fighters is not an amiibo, exiting."):
