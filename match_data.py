7ad4765015c0ef326ec076959f6f9a18b66f71ce
jozz024 <74272560+jozz024@users.noreply.github.com>
2024-02-18 08:56:10 -0600

    13.0.2

--- match_data.py
+++ match_data.py
@@ -33,9 +33,9 @@ def export_result_to_csv(result_data, tournament_name, fp1, fp2):
         winner = lambda fp1_name, result, fp2_name: fp1_name if result == 1 else fp2_name
         outcome = lambda p1_score, p2_score: f"{p1_score}:{p2_score}" if p1_score > p2_score else f"{p2_score}:{p1_score}"
         if x == max_index - 1:
-            data.writerow([tournament_name, fp1_name, fp2_name, winner(fp1_name, result, fp2_name), outcome(p1_score, p2_score), result_data["stocks_taken"][x][0], result_data["stocks_taken"][x][1], stage_translate_dict[result_data["stages_used"][x]]])
+            data.writerow([tournament_name, fp1_name, fp2_name, winner(fp1_name, result, fp2_name), outcome(p1_score, p2_score), result_data["stocks_taken"][x][0], result_data["stocks_taken"][x][1], result_data["stages_used"][x]])
         else:
-            data.writerow([tournament_name, fp1_name, fp2_name, winner(fp1_name, result, fp2_name), "", result_data["stocks_taken"][x][0], result_data["stocks_taken"][x][1], stage_translate_dict[result_data["stages_used"][x]]])
+            data.writerow([tournament_name, fp1_name, fp2_name, winner(fp1_name, result, fp2_name), "", result_data["stocks_taken"][x][0], result_data["stocks_taken"][x][1], result_data["stages_used"][x]])
 
 
 
